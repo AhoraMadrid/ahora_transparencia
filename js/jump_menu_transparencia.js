1fcb09b94fe1625d66b147a299f049eca28c7d03
suela <dsuela@xsto.info>
2016-03-14 10:50:38 +0100

    Adaptando al wordpress...

--- js/jump_menu_transparencia.js
+++ js/jump_menu_transparencia.js
@@ -1,14 +1,15 @@
 // create menu
 $('#jump_menu').append( "<option value='index.html'>Seleccione una opción</option>" );
-$('#jump_menu').append( "<option value='municipales_2015.html'>Campaña Elecciones Municipales 2015</option>" );
-$('#jump_menu').append( "<option value='salarios.html'>Salario de Concejales</option>" );
-$('#jump_menu').append( "<option value='eventual.html'>Salarios Personal eventual</option>" );
+$('#jump_menu').append( "<option value='/transparencia/presupuesto-elecciones-municipales-2015/'>Campaña Elecciones Municipales 2015</option>" );
+$('#jump_menu').append( "<option value='/transparencia/salarios-concejales/'>Salario de Concejales</option>" );
+$('#jump_menu').append( "<option value='/transparencia/salarios-personal-eventual/'>Salarios Personal eventual</option>" );
 $('#jump_menu').append( "<option value='balance_2015.html' disabled>Gastos e Ingresos 2015</option>" );
 
 // jump function
 $('#jump_menu').change( function() {
 	var nextPage = $(this).find(":selected").val();
-	//console.log(nextPage);
-	$(location).attr('href',nextPage);
-    
+	console.log(nextPage);
+	//window.parent.jQuery(location).attr('href',nextPage);
+        top.window.location.href = nextPage;
+        console.log('MENU');
 })
